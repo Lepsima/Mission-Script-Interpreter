f221f2a90a9d82ad241d12617d1b9acfb99a5b44
Lepisma <hugo.diaz.morcillo@gmail.com>
2026-01-09 11:14:55 +0100

    Update STCRImporter.cs

--- Unity/Editor/STCRImporter.cs
+++ Unity/Editor/STCRImporter.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEditor.AssetImporters;
 using UnityEngine;
+using System.IO;
 
 [ScriptedImporter(1, "stcr")]
 public class STCRImporter : ScriptedImporter {
 	public override void OnImportAsset(AssetImportContext ctx) {
 		// ReSharper disable once RedundantNameQualifier
-		string text = System.IO.File.ReadAllText(ctx.assetPath);
+		string text = File.ReadAllText(ctx.assetPath);
 		TextAsset asset = new(text);
 		ctx.AddObjectToAsset("main", asset);
 		ctx.SetMainObject(asset);
